d2e585bd5655789970ec322df275a782750a6b39
luis carlos <luiscrueda.ing@gmail.com>
2026-06-02 18:49:25 -0500

    responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,9 @@ import Video from '../video/Video'
 
 const Header = () => {
   return (
-    <div className='header '>
+    <div className='header'>
       <div className="container header__container " >
-          <section className='header__left flex__center'>
+          <section className='header__left flex__center '>
             <h1 >{MAINHEADER[0].title}</h1>
              <div className="header__image ">
               <img src={MAINHEADER[0].imgUrl} alt="image" />
